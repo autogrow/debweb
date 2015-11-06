0efab0fd4f54d8ec0f9d8912266e14a3eaf9c48d
Robert McLeod <robert@autogrow.com>
2015-11-07 00:22:45 +1300

    fleshed out some views

--- app/controllers/branches_controller.rb
+++ app/controllers/branches_controller.rb
@@ -69,7 +69,7 @@ class BranchesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_branch
-      @branch = Branch.find(params[:id] || params[:branch_id)
+      @branch = Branch.find(params[:id] || params[:branch_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
