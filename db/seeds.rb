015e18f9e2f3846df6c92e774feca19343f09cbb
user.email <robert@penguinpower.co.nz>
2016-01-26 20:53:56 +1300

    added myself to the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+User.new(email: "robert@penguinpower.co.nz", password: "11111111").save!
