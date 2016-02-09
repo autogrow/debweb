00de52434526b3efeed69b37966f2a936f8e4aa5
Robert McLeod <robert@autogrow.com>
2016-02-09 16:18:46 +1300

    packages helper link was breaking the packages page somehow

--- app/helpers/packages_helper.rb
+++ app/helpers/packages_helper.rb
@@ -1,6 +1,7 @@
 module PackagesHelper
   def auto_add_package_label(package)
     icon, klass = *(package.branch.auto_added?(package.name) ? ['ok-circle', 'success'] : ['ban-circle', 'default'])
-    raw link_to(glyph(icon), auto_add_package_branch_path(package_id: package.id), class: "btn btn-sm btn-#{klass}")
+    raw link_to(glyph(icon), '#', class: "btn btn-sm btn-#{klass}")
+    #auto_add_package_branch_path(package_id: package.id)
   end
 end
